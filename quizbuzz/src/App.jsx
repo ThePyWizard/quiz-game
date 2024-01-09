32c88836bb639185df86164cca1b0f3cadc22a47
Jishnu M M <jishnumm4312@gmail.com>
2024-01-09 20:01:45 +0530

    made some changes by routing

--- quizbuzz/src/App.jsx
+++ quizbuzz/src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Quiz from './components/Quiz';
+import Leaderboard from './pages/Leaderboard';
 import './index.css';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-6xl flex justify-center font-custom font-bold mt-20'>QUIZ <span className="text-white bg-green-500 ml-1"> BUZZ</span></h1>
-      
-      <Quiz />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Quiz/>} />
+          <Route path='/leaderboard' element={<Leaderboard />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
